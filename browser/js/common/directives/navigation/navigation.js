a37eeff0e453f7843a6f278ccaafe7571ef3c2df
Nicholas Palenchar <nick@Nicks-pro.local>
2016-05-07 22:49:08 -0400

    menu

---
+++ browser/js/common/directives/navigation/navigation.js
@@ -0,0 +1,32 @@
+app.directive('navigation', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'js/common/directives/navigation/navigation.html',
+    controller: ctrl
+  };
+});
+
+function ctrl($scope) {
+  $scope.thing = "p.ueo";
+  $scope.items = [
+    {
+      icon: "view_list",
+      name: "Gradebook"
+    }, {
+      icon: "face",
+      name: "Students"
+    },
+    {
+      icon: "people",
+      name: "Classes"
+    },
+    {
+      icon: "import_contacts",
+      name: "Courses"
+    },
+    {
+      icon: "description",
+      name: "Assignments"
+    },
+  ]
+}
